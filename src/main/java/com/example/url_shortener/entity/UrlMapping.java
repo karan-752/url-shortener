f09c9afc910a862069f7108bbc7a4e78c8e0d0ad
Karan Gupta <karangupta752@gmail.com>
2026-06-14 12:10:36 +0530

    Setup Spring Boot project with PostgresSOQL Integration

---
+++ src/main/java/com/example/url_shortener/entity/UrlMapping.java
@@ -0,0 +1,49 @@
+package com.example.url_shortener.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class UrlMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String originalURL;
+
+    @Column(unique = true)
+    private String shortCode;
+
+    private LocalDateTime createdAt;
+
+    public UrlMapping() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getOriginalUrl() {
+        return originalURL;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalURL = originalUrl;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
